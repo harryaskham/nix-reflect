7cd35e15c5e2aed9a1c70582a89d532509521083
Harry Askham <harryaskham@gmail.com>
2025-08-15 23:17:15 +0100

    [rapid commit; squash later]

--- lib/eval/monad.nix
+++ lib/eval/monad.nix
@@ -426,8 +426,6 @@ rec {
           __toString = self: _b_ "Eval ${A} (${_ph_ self.e})";
           inherit S E A s e;
 
-          strictState = true;
-
           # modify :: (EvalState -> EvalState) -> Eval A -> Eval {}
           modify = f: 
             if isLeft this.e then this else
@@ -440,13 +438,8 @@ rec {
           # Thunked to avoid infinite nesting - (m.get {}) is an (Eval EvalState)
           get = {_, ...}: _.pure (this.s (S.mempty {}));
 
-          setState = s: (Eval A s this.e).maybeForceState {};
-          mapState = f: (Eval A (f this.s) this.e).maybeForceState {};
-          maybeForceState = {}:
-            if this.strictState then this.forceState {} else this;
-          forceState = {}:
-            let state = this.s (S.mempty {});
-            in Eval A (const state) this.e;
+          setState = s: Eval A s this.e;
+          mapState = f: Eval A (f this.s) this.e;
 
           mapEither = f: 
             let e = f this.e;
