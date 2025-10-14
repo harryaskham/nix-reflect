dca565a0790c3f12807c4e185e192136da4fe7a7
Harry Askham <harryaskham@gmail.com>
2025-10-14 10:48:49 +0100

    [rapid commit; squash later]

--- flake.nix
+++ flake.nix
@@ -2,18 +2,14 @@
   description = "Nix library for parsing, evaluating and reflecting on Nix code, written in pure Nix";
 
   inputs = {
-    #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    #flake-utils.url = "github:numtide/flake-utils";
-    #nix-parsec.url = "github:nprindle/nix-parsec";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nix-parsec.url = "github:nprindle/nix-parsec";
     collective-public = {
-      url = "git+ssh://git@github.com/harryaskham/collective-public";
+      url = "github:harryaskham/collective-public";
       inputs.nix-reflect.follows = "";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nixpkgs.url = "git+https://git@github.com/nixos/nixpkgs?ref=nixos-unstable";
-    nixpkgs-stable.url = "git+https://git@github.com/nixos/nixpkgs?ref=release-25.05";
-    nix-parsec.url = "git+https://git@github.com/nprindle/nix-parsec";
   };
 
   outputs = { self, nixpkgs, flake-utils, collective-public, nix-parsec, ... } @ inputs:
